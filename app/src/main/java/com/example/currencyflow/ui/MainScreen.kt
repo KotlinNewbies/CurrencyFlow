bf03739b5231177c71227134be372aa7c1836a5a
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2024-05-28 11:47:02 +0200

    Removed unused import directives

--- app/src/main/java/com/example/currencyflow/ui/MainScreen.kt
+++ app/src/main/java/com/example/currencyflow/ui/MainScreen.kt
@@ -19,9 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
--- app/src/main/java/com/example/currencyflow/ui/components/DropDownMenu.kt
+++ app/src/main/java/com/example/currencyflow/ui/components/DropDownMenu.kt
@@ -1,6 +1,5 @@
 package com.example.currencyflow.ui.components
 
-import android.media.Image
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,13 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-import com.example.currencyflow.R
 import com.example.currencyflow.classes.Currency
 
 @Composable
