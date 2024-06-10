caab191076220982c019c20524c9c0eb97c92a2f
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2024-06-10 21:41:03 +0200

    repairs

--- app/src/main/java/com/example/currencyflow/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyflow/ui/MainActivity.kt
@@ -20,12 +20,6 @@ import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Sprawdzenie czy plik istnieje przy starcie aplikacji
-        val fileName = "user_data.json"
-        val file = File(filesDir, fileName)
-        if (!file.exists()) {
-            saveData(this) // Zapisz plik jeśli plik nie istnieje
-        }
         super.onCreate(savedInstanceState)
 
         // Sprawdzenie czy plik istnieje przy starcie aplikacji
