7b14ddb9b7014ef36128543c8af1eae2b635763a
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2024-07-10 10:21:26 +0200

    favorite screen with save/load functions

--- app/src/main/java/com/example/currencyflow/ui/FavCurrencies.kt
+++ app/src/main/java/com/example/currencyflow/ui/FavCurrencies.kt
@@ -1,16 +1,14 @@
 package com.example.currencyflow.ui
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,28 +16,31 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.currencyflow.R
 import com.example.currencyflow.classes.Currency
+import com.example.currencyflow.data.data_management.loadSelectedCurrencies
+import com.example.currencyflow.data.data_management.saveSelectedCurrencies
 
 @Composable
 fun FavCurrencies(navController: NavController) {
-    val sampleCurrencies = listOf(
-        Currency.USD,
-        Currency.JPY
-    )
+    val context = LocalContext.current
+    val allCurrencies = Currency.entries.toList()
+    val initialSelectedCurrencies = loadSelectedCurrencies(context)
+    val selectedCurrencies = remember { mutableStateMapOf<Currency, Boolean>().apply {
+        initialSelectedCurrencies.forEach { put(it, true) }
+    }}
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -47,24 +48,23 @@ fun FavCurrencies(navController: NavController) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(modifier = Modifier
-            .width(400.dp)
-            .height(600.dp)
-            .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            items(allCurrencies) { currency ->
+                val isSelected = selectedCurrencies[currency] ?: false
+                CurrencyItem(currency = currency, isSelected = isSelected) { selected ->
+                    selectedCurrencies[currency] = selected
+                }
+            }
         }
-//        LazyColumn {
-//            items(sampleCurrencies) { currency ->
-//                CurrencyItem(currency = currency) {
-//                    // Handle currency click
-//                }
-//            }
-//        }
         Button(
             onClick = {
-                navController.navigateUp() // Go back to the previous screen
+                val selectedCurrencyList = selectedCurrencies.filterValues { it }.keys.toList()
+                saveSelectedCurrencies(context, selectedCurrencyList)
+                navController.navigateUp() // Powrót do poprzedniego ekranu
             }
         ) {
             Text(text = "Zapisz")
@@ -72,12 +72,45 @@ fun FavCurrencies(navController: NavController) {
     }
 }
 
-@Preview
 @Composable
-fun FavCurrenciesPreview() {
-    val sampleCurrencies = listOf(
-        Currency.USD,
-        //Currency.EUR,
-        Currency.JPY
-    )
+fun CurrencyItem(
+    currency: Currency,
+    isSelected: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    rememberCoroutineScope()
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null, // Wyłączamy domyślny feedback
+                onClick = {
+                    onCheckedChange(!isSelected)
+                }
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Image(
+                modifier = Modifier.size(36.dp),
+                painter = painterResource(id = currency.icon),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = currency.symbol)
+        }
+        Checkbox(
+            checked = isSelected,
+            onCheckedChange = null, // Ustawiamy null, aby uniknąć interakcji z checkboxem bezpośrednio
+            modifier = Modifier.wrapContentSize() // Aby zachować odpowiedni rozmiar wiersza
+        )
+    }
 }
