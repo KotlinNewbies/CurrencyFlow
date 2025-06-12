21e61a9c8cd00c571e45bd165a1f3007e05090cd
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2025-06-12 14:15:59 +0200

    Support Android 14 language settings
    Adds `localeConfig` to AndroidManifest.xml and sets `tools:targetApi="35"` to enable per-app language preferences on Android 14 and above.
    
    Removes an unnecessary comment in `CurrencyFlowApplication.kt`.

--- app/src/main/java/com/example/currencyflow/CurrencyFlowApplication.kt
+++ app/src/main/java/com/example/currencyflow/CurrencyFlowApplication.kt
@@ -1,9 +1,7 @@
-package com.example.currencyflow // Zastąp 'com.example.currencyflow' nazwą Twojego pakietu
+package com.example.currencyflow
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CurrencyFlowApplication : Application() {
-
-}
+class CurrencyFlowApplication : Application()
